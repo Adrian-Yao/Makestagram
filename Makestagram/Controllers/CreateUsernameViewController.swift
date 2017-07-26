b7b7ed73929c89bf7179fafcc443b98a2bc38fba
Adrian-Yao <adrian.c.yao@gmail.com>
2017-07-26 16:20:49 -0400

    Up to "Writing Data to Firebase"

---
+++ Makestagram/Controllers/CreateUsernameViewController.swift
@@ -0,0 +1,26 @@
+//
+//  CreateUsernameViewController.swift
+//  Makestagram
+//
+//  Created by adyao20 on 7/26/17.
+//  Copyright © 2017 adyao20. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CreateUsernameViewController: UIViewController {
+    
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var nextButton: UIButton!
+ 
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    nextButton.layer.cornerRadius = 6
+    }
+    
+    
+    @IBAction func nextButtonTapped(_ sender: UIButton) {
+    }
+}
--- Makestagram/Controllers/LoginViewController.swift
+++ Makestagram/Controllers/LoginViewController.swift
@@ -7,6 +7,12 @@
 //
 
 import UIKit
+import FirebaseAuth
+import FirebaseAuthUI
+import FirebaseDatabase
+
+
+typealias FIRUser = FirebaseAuth.User
 
 class LoginViewController: UIViewController {
 
@@ -23,6 +29,17 @@ class LoginViewController: UIViewController {
     }
     
     @IBAction func loginButtonTapped(_ sender: Any) {
+        // 1
+        guard let authUI = FUIAuth.defaultAuthUI()
+            else { return }
+        
+        // 2
+        authUI.delegate = self
+        
+        // 3
+        let authViewController = authUI.authViewController()
+        present(authViewController, animated: true)
+        
         print("login button tapped")
     }
     /*
@@ -35,4 +52,34 @@ class LoginViewController: UIViewController {
     }
     */
 
+}
+
+extension LoginViewController: FUIAuthDelegate {
+    func authUI(_ authUI: FUIAuth, didSignInWith user: FIRUser?, error: Error?) {
+        if let error = error {
+            assertionFailure("Error signing in: \(error.localizedDescription)")
+            return
+        }
+        // 1
+        guard let user = user
+            else { return }
+        
+        // 2
+        let userRef = Database.database().reference().child("users").child(user.uid)
+        
+        // 3
+        userRef.observeSingleEvent(of: .value, with: { (snapshot) in
+            // retrieve user data from snapshot
+            // 1
+            userRef.observeSingleEvent(of: .value, with: { [unowned self] (snapshot) in
+                if let user = User(snapshot: snapshot) {
+                    print("Welcome back, \(user.username).")
+                } else {
+                    self.performSegue(withIdentifier: "toCreateUsername", sender: self)
+                }
+            })
+        
+        print("handle user signup / login")
+    }
+        )}
 }
