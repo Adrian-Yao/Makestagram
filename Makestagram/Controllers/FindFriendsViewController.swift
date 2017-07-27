fa32c91a45a85b6254353aa4fe06627312cca070
Adrian-Yao <adrian.c.yao@gmail.com>
2017-07-27 16:53:50 -0400

    Finished Step 11

---
+++ Makestagram/Controllers/FindFriendsViewController.swift
@@ -0,0 +1,9 @@
+//
+//  FindFriendsViewController.swift
+//  Makestagram
+//
+//  Created by adyao20 on 7/27/17.
+//  Copyright © 2017 adyao20. All rights reserved.
+//
+
+import Foundation
---
+++ Makestagram/Controllers/HomeViewController.swift
@@ -0,0 +1,9 @@
+//
+//  HomeViewController.swift
+//  Makestagram
+//
+//  Created by adyao20 on 7/27/17.
+//  Copyright © 2017 adyao20. All rights reserved.
+//
+
+import Foundation
